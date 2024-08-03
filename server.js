8cfa545e155255fe0d384e244afbcf8242d13f40
Naiara Pacheco <naiarapachecosantos@gmail.com>
2024-08-03 20:04:38 -0300

    feat: novos metodos e  parametros

--- server.js
+++ server.js
@@ -1,29 +1,75 @@
-import express from 'express'
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
 
-const app = express()
-app.use(express.json())
+const prisma = new PrismaClient();
 
-const users = []
+const app = express();
+app.use(express.json());
 
-app.post('/usuarios', (req, res) => {
+const users = [];
 
-    users.push(req.body)
+app.post('/usuarios', async (req, res) => {
 
-    res.status(201).json(req.body)
+    await prisma.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age,
+        }
+    })
+
+    res.status(201).json(req.body);
 
 })
 
-app.get('/usuarios', (req, res) => {
-    res.status(200).json(users)
+app.get('/usuarios', async (req, res) => {
+
+    let users = [];
+
+    if (req.query){
+        users = await prisma.user.findMany({
+            where:{
+                name: req.query.name,
+                email: req.query.email,
+                age: req.query.age
+            }
+        })
+    } else {
+        users = await prisma.user.findMany();
+    }
+
+    res.status(200).json(users);
+
 });
+
+app.put('/usuarios/:id', async (req, res) => {
+
+    await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age,
+        }
+    })
+
+    res.status(201).json(req.body);
+
+});
+
+app.delete('/usuarios/:id', async (req, res) => {
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        }
+    });
+
+    res.status(200).json({message: "Usuário deletado com sucesso!"});
+
+})
 
 app.listen(3000);
 
 
-
-/* Criar nossa api de usuarios
-    - criar um usuario
-    - listar todos os usuarios
-    - editar um usuario
-    - deletar um usuario
-*/
